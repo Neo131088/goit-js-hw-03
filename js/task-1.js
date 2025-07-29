90b7787363cdcaa80780b18f13f289c18c23dead
Neo131088 <neo131088@gmail.com>
2025-07-29 16:21:31 +0200

    add123

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const slugify = function (title) {
+  const slug = title.split(' ').join('-').toLowerCase();
+  return slug;
+};
+
+
+console.log(slugify('Arrays for beginners'));
+console.log(slugify('English for developer'));
+console.log(slugify('Ten secrets of JavaScript'));
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
